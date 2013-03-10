e85e76a8e11bfacac33d64dc4e8ab74fbd3a0f1e
Philip Jägenstedt <philip@foolip.org>
2013-03-10 17:32:55 +0700

    Include the images in the EPUB

--- epub.sh
+++ epub.sh
@@ -2,6 +2,10 @@
 #
 # Make stagestosaturn.epub
 
+cd "$(dirname $0)"
+
 rm -f stagestosaturn.epub
+rm -f OEBPS/p*.jpg
+cp p*.jpg OEBPS/
 zip -X stagestosaturn.epub mimetype
 zip -rg stagestosaturn.epub META-INF OEBPS -x \*~ \*.DS_Store \*.gitignore

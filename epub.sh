6d6e195361c772e8afe9e54f218deea071ee74c2
Philip Jägenstedt <philip@foolip.org>
2013-03-10 15:47:38 +0700

    Simplify the EPUB structure; add TOC
    main.html needs to be generated by santize.html and copied into place

--- epub.sh
+++ epub.sh
@@ -4,4 +4,4 @@
 
 rm -f stagestosaturn.epub
 zip -X stagestosaturn.epub mimetype
-zip -rg stagestosaturn.epub META-INF OEBPS -x \*~ \*.DS_Store
+zip -rg stagestosaturn.epub META-INF OEBPS -x \*~ \*.DS_Store \*.gitignore

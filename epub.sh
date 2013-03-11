a8a3b4f0544573cbc9d7038f99deee733fc30247
Philip Jägenstedt <philip@foolip.org>
2013-03-11 11:08:43 +0700

    Make a new high-resolution cover image
    cover-hires.jpg is a retouched version of
    http://ia600308.us.archive.org/BookReader/BookReaderImages.php?zip=/6/items/stagestosaturnte00bilsrich/stagestosaturnte00bilsrich_jp2.zip&file=stagestosaturnte00bilsrich_jp2/stagestosaturnte00bilsrich_0001.jp2
    
    cover.jpg is a downscaled version for EPUB

--- epub.sh
+++ epub.sh
@@ -5,6 +5,6 @@
 cd "$(dirname $0)"
 
 rm -f stagestosaturn.epub
-cp stylesheet.css p*.jpg OEBPS/
+cp cover.jpg stylesheet.css p*.jpg OEBPS/
 zip -X stagestosaturn.epub mimetype
 zip -rg stagestosaturn.epub META-INF OEBPS -x \*~ \*.DS_Store \*.gitignore

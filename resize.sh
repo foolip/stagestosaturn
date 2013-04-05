b4bcc58e234c2ccd0ed8b55a826c49a1cff713db
Philip Jägenstedt <philip@foolip.org>
2013-04-05 17:05:08 +0700

    Add support for a .args file in resize.sh

--- resize.sh
+++ resize.sh
@@ -8,13 +8,16 @@ maxw="$3"
 maxh="$4"
 
 identify -format "%w %h" "$src" | while read w h; do
+    argf="${src/%jpg/args}"
     crop="${src/%jpg/crop}"
-    if [ -e "$crop" ]; then
+    if [ -e "$argf" ]; then
+	read args < "$argf"
+    elif [ -e "$crop" ]; then
 	read top right bottom left < "$crop"
 	args="-crop $(($w-$left-$right))x$(($h-$top-$bottom))+$left+$top"
     fi
     if [ -n "$args" -o $w -gt $maxw -o $h -gt $maxh ]; then
-	convert "$src" $args -resize "${maxw}x${maxh}>" "$dst"
+	convert "$src" $args +repage -resize "${maxw}x${maxh}>" "$dst"
     else
 	cp "$src" "$dst"
     fi
